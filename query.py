d8e2a483ffcf1282b7b4a800bc9e3f409c2712ae
Alison Langston <alangsto@wellesley.edu>
2019-06-10 11:51:04 -0400

    updated to python3

--- query.py
+++ query.py
@@ -18,18 +18,18 @@ domain_output = set()
 
 # Start scrolling
 while (scroll_size > 0):
-    print "Scrolling..."
+    print("Scrolling...")
     page = es.scroll(scroll_id = sid, scroll = '2m')
     # Update the scroll ID
     sid = page['_scroll_id']
     # Get the number of results that we returned in the last scroll
     scroll_size = len(page['hits']['hits'])
-    print "scroll size: " + str(scroll_size)
+    print("scroll size: " + str(scroll_size))
     for elt in page['hits']['hits']:
         #print ("domain: %s \n" % elt['_source']['author_domain'])
         domain_output.add(elt['_source']['author_domain'])
 
 for elt in domain_output:
-    print ("%s \n" % elt)
+    print("%s \n" % elt)
 
-print len(domain_output)
+print(len(domain_output))
