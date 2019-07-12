9c4feb88da48f3a05f76053bd8a349678eeec2eb
Alison Langston <alangsto@wellesley.edu>
2019-07-12 10:56:57 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /
 
-/usr/bin/python3 /set_config.py 
+sudo /usr/bin/python3 /set_config.py 
 #/usr/bin/python3 /create_dashboard.py -r /og_projects.json -wr /projects.json -cf /override.cfg
 
 #sleep 5
