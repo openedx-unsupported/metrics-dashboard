88dfa5a0ea8cfac32218ef6e02b971edffe5a041
Alison Langston <alangsto@wellesley.edu>
2019-07-12 13:17:34 -0400

    finalized entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 cd /
-
 sudo /usr/bin/python3 /set_config.py $GITHUB_KEY $SLACK_KEY $DB_USER $DB_PASS $DB_HOST $DB_NAME $ELASTIC_URL $KIBANA_URL
 sudo /usr/bin/python3 /create_dashboard.py -r /og_projects.json -wr /projects.json -cf /override.cfg
-
 /usr/local/bin/sirmordred $*
-#sleep 5
