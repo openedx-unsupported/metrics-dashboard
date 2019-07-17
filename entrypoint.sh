865651cc9353101d26e0b9e22e7c3ef8a5797cdd
Alison Langston <alangsto@wellesley.edu>
2019-07-17 14:07:49 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cd /
 sudo /usr/bin/python3 /set_config.py $GITHUB_KEY $SLACK_KEY $DB_USER $DB_PASS $DB_HOST $DB_NAME $ELASTIC_URL $KIBANA_URL
 sudo /usr/bin/python3 /create_dashboard.py -r /og_projects.json -wr /projects.json -cf /override.cfg
 
-sudo -e git clone https://alangsto:$GITHUB_KEY@github.com/edx/repo-tools-data.git
+sudo git clone https://alangsto:$GITHUB_KEY@github.com/edx/repo-tools-data.git
 cp repo-tools-data/people.yaml .
 rm -rf repo-tools-data
 sudo /usr/bin/python3 /create_identities.py
