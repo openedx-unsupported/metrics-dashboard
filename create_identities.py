b6b223cce2ee7e5e553c2c5efd028537a323b0b0
Alison Langston <alangsto@wellesley.edu>
2019-07-17 13:57:44 -0400

    added script for converting yaml files

---
+++ create_identities.py
@@ -0,0 +1,40 @@
+import yaml
+
+with open("people.yaml", 'r') as stream:
+    try:
+        old_yaml = yaml.safe_load(stream)
+        data = []
+        for key in old_yaml:
+        	entry = {
+        				'profile': {'name': ''}, 
+        				'enrollments': [{'organization': ''}],
+        				'email': [],
+        				'github': []
+        			}
+
+        	#PROFILE/NAME
+        	entry['profile']['name'] = old_yaml[key]['name']
+
+        	#INSTITUTIONS
+        	#need to fix in case of more institutions
+        	if 'institution' in old_yaml[key]:
+        		entry['enrollments'][0]['organization'] = old_yaml[key]['institution']
+        	else:
+        		entry['enrollments'][0]['organization'] = 'Independent'
+
+        	#EMAILS
+        	entry['email'].append(old_yaml[key]['email'])
+        	if 'other_emails' in old_yaml[key]: #in case there are other emails
+        		for email in old_yaml[key]['other_emails']:
+        			entry['email'].append(email)
+  
+        	#GITHUB
+        	entry['github'].append(key)
+
+        	data.append(entry)
+        
+        with open('identities.yml', 'w') as outfile:
+        	yaml.dump(data, outfile, default_flow_style=False)
+        	
+    except yaml.YAMLError as exc:
+        print(exc)
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,10 @@
 cd /
 sudo /usr/bin/python3 /set_config.py $GITHUB_KEY $SLACK_KEY $DB_USER $DB_PASS $DB_HOST $DB_NAME $ELASTIC_URL $KIBANA_URL
 sudo /usr/bin/python3 /create_dashboard.py -r /og_projects.json -wr /projects.json -cf /override.cfg
+
+git clone https://alangsto:$GITHUB_KEY@github.com/edx/repo-tools-data.git
+cp repo-tools-data/people.yaml .
+rm -rf repo-tools-data
+sudo /usr/bin/python3 /create_identities.py
+
 /usr/local/bin/sirmordred $*
