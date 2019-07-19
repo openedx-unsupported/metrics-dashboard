6fd1523d01343425481e5a3474742078ad6e77f7
Alison Langston <alangsto@wellesley.edu>
2019-07-19 09:02:12 -0400

    adjusted timing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 cd /
+echo "Configuring credentials . . ."
 sudo /usr/bin/python3 /set_config.py $GITHUB_KEY $SLACK_KEY $DB_USER $DB_PASS $DB_HOST $DB_NAME $ELASTIC_URL $KIBANA_URL
+echo "Loading projects . . . "
 sudo /usr/bin/python3 /create_dashboard.py -r /og_projects.json -wr /projects.json -cf /override.cfg
+sleep 5
 
+echo "Loading identities . . . "
 sudo git clone https://alangsto:$GITHUB_KEY@github.com/edx/repo-tools-data.git
 sudo cp repo-tools-data/people.yaml .
 sudo rm -rf repo-tools-data
 sudo /usr/bin/python3 /create_identities.py
+
+sleep 5
 
 /usr/local/bin/sirmordred $*
