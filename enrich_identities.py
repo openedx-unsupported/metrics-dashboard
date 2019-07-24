7e0e2438508a8c84a9c459315f1484906d3e4530
Alison Langston <alangsto@wellesley.edu>
2019-07-24 16:49:30 -0400

    basic enrichment for git author emails

---
+++ enrich_identities.py
@@ -0,0 +1,44 @@
+"""
+This script was created in order to better identify people
+based on the various emails they use to write git commits,
+which may differ from the email they use 
+"""
+
+from github import Github
+import yaml
+import os
+
+with open('people.yaml', 'r') as stream:
+    try:
+        people = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+gh = Github(os.environ['GITHUB_KEY'])
+repos = gh.get_organization('edX').get_repos('public')
+for repo in repos:
+    try:
+        commits = repo.get_commits()
+        for commit in commits:
+
+            try: #if the username is still active
+                username = commit.author.login
+                git_author_email = commit.commit.author.email
+                contributor = people[username]
+                if contributor['email'] != git_author_email:
+                    print(git_author_email)
+                    if 'other_emails' in contributor and git_author_email not in contributor['other_emails']:
+                        contributor['other_emails'].append(git_author_email)
+                    if 'other_emails' not in contributor:
+                        contributor.update({'other_emails': [git_author_email]})
+
+            except: #inactive username, can't parse data
+                i = 0
+
+    except:
+        print('Empty repository')
+        print()
+    break
+
+with open('result.yaml', 'w') as outfile:
+        yaml.dump(people, outfile, default_flow_style=False)
