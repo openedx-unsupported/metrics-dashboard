1ddf0754779566082e30bdab8b2ffaaba07eca6a
Alison Langston <alangsto@wellesley.edu>
2019-07-30 15:57:39 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Starting . . . "
 cd /
 echo "Configuring credentials . . ."
-/usr/bin/python3 /set_config.py $GITHUB_KEY $SLACK_KEY $DB_USER $DB_PASS $DB_HOST $DB_NAME $ELASTIC_URL $KIBANA_URL
+sudo /usr/bin/python3 /set_config.py $GITHUB_KEY $SLACK_KEY $DB_USER $DB_PASS $DB_HOST $DB_NAME $ELASTIC_URL $KIBANA_URL
 echo "Loading projects . . . "
 /usr/bin/python3 /create_dashboard.py -r /og_projects.json -wr /projects.json -cf /override.cfg
 sleep 5
