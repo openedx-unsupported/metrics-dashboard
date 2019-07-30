0232a267be1d84606f2c1b74317b79a627c1cca9
Alison Langston <alangsto@wellesley.edu>
2019-07-30 16:00:26 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,15 @@ cd /
 echo "Configuring credentials . . ."
 sudo /usr/bin/python3 /set_config.py $GITHUB_KEY $SLACK_KEY $DB_USER $DB_PASS $DB_HOST $DB_NAME $ELASTIC_URL $KIBANA_URL
 echo "Loading projects . . . "
-/usr/bin/python3 /create_dashboard.py -r /og_projects.json -wr /projects.json -cf /override.cfg
+sudo /usr/bin/python3 /create_dashboard.py -r /og_projects.json -wr /projects.json -cf /override.cfg
 sleep 5
 
-git clone https://alangsto:$GITHUB_KEY@github.com/edx/repo-tools-data.git
-cp repo-tools-data/people.yaml .
-rm -rf repo-tools-data
+sudo git clone https://alangsto:$GITHUB_KEY@github.com/edx/repo-tools-data.git
+sudo cp repo-tools-data/people.yaml .
+sudo rm -rf repo-tools-data
 echo "Loading identities . . . "
-/usr/bin/python3 /create_identities.py
+sudo /usr/bin/python3 /create_identities.py
 
 sleep 5
 
-/usr/local/bin/sirmordred -c /infra.cfg /dashboard.cfg /project.cfg /override.cfg
+sudo /usr/local/bin/sirmordred -c /infra.cfg /dashboard.cfg /project.cfg /override.cfg
