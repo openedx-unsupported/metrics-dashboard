baf3375e83cc3713b454e310ecb70baf340f2073
Alison Langston <alangsto@wellesley.edu>
2019-07-31 15:00:33 -0400

    cleaned up code

--- enrich_identities.py
+++ enrich_identities.py
@@ -8,6 +8,46 @@ import github3
 import yaml
 import os
 
+def read_file():
+    with open('people.yaml', 'r') as stream:
+        try:
+            people = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)    
+    return people
+
+def enrich(data):
+    gh = github3.login('alangsto', token=os.environ['GITHUB_KEY'])
+    repos = gh.organization('edX').repositories('public')
+    emails = []
+    for repo in repos:
+        if not repo.fork: #only look at repositories that are not forks
+            try:
+                commits = repo.commits()
+                for git_commit in commits:
+                    try: #if the username is still active
+                        username = git_commit.author.login
+                        add_email(username, git_commit)
+
+                    except AttributeError as exc: 
+                        #then we want to look at name, and see if we can assign an email to an existing name
+                        for name in data:
+                            person = data[name]
+                            if person['name'] == git_commit.commit.author['name']:
+                                add_email(name, git_commit)
+                    except KeyError:
+                        #happens for usernames that aren't included in our people.yaml file
+                        pass
+
+            except github3.exceptions.ForbiddenError as exc:
+                #if we hit a rate limit, still want to write to the file
+                print(exc)
+                return data
+            except github3.exceptions.Conflict:
+                #if there is an empty repo
+                pass
+    return data
+
 def add_email(username, git_commit):
     git_author_email = git_commit.commit.author['email']#see if you can get email from commit instead of git commit
     contributor = people[username]
@@ -21,47 +61,8 @@ def write_file(data):
     with open('result.yaml', 'w') as outfile:
         yaml.dump(data, outfile, default_flow_style=False)
 
-people = {}
+if __name__ == '__main__':
 
-#first load the people.yaml file
-with open('people.yaml', 'r') as stream:
-    try:
-        people = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
-
-gh = github3.login('alangsto', token=os.environ['GITHUB_KEY'])
-repos = gh.organization('edX').repositories('public')
-emails = []
-for repo in repos:
-    if not repo.fork: #only look at repositories that are not forks
-        try:
-            commits = repo.commits()
-            for git_commit in commits:
-                try: #if the username is still active
-                    username = git_commit.author.login
-                    add_email(username, git_commit)
-
-                except AttributeError as exc: 
-                    #then we want to look at name, and see if we can assign an email to an existing name
-                    for name in people:
-                        person = people[name]
-                        if person['name'] == git_commit.commit.author['name']:
-                            add_email(name, git_commit)
-                except KeyError:
-                    #happens for usernames that aren't included in our people.yaml file
-                    print('Key Error: ' + git_commit.author.login + ", " + git_commit.commit.author['name'])
-                    print(git_commit.html_url)
-                    print()
-
-        except github3.exceptions.ForbiddenError as exc:
-            #if we hit a rate limit, still want to write to the file
-            write_file(people)
-            print(exc)
-            break
-        except github3.exceptions.Conflict:
-            #if there is an empty repo
-            pass
-
-#create new yaml file with same format
-write_file(people)
+    people = read_file()
+    enriched_data = enrich(people)
+    write_file(enriched_data)
