2abf054c740c95529a755a557ea1ea9aff50c18e
Alison Langston <alangsto@wellesley.edu>
2019-08-07 09:15:02 -0400

    updated for Heroku

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,16 @@
 echo "Starting GrimoireLabs. . . "
 cd /
 echo "Configuring credentials . . ."
-sudo /usr/bin/python3 /set_config.py $GITHUB_KEY $SLACK_KEY $DB_USER $DB_PASS $DB_HOST $DB_NAME $ELASTIC_URL $KIBANA_URL
+/usr/bin/python3 /set_config.py $GITHUB_KEY $SLACK_KEY $DB_USER $DB_PASS $DB_HOST $DB_NAME $ELASTIC_URL $KIBANA_URL
 echo "Loading projects . . . "
-sudo /usr/bin/python3 /create_dashboard.py -r /og_projects.json -wr /projects.json -cf /override.cfg
+/usr/bin/python3 /create_dashboard.py -r /og_projects.json -wr /projects.json -cf /override.cfg
 sleep 5
 
-sudo git clone https://alangsto:$GITHUB_KEY@github.com/edx/repo-tools-data.git
-sudo cp repo-tools-data/people.yaml .
-sudo rm -rf repo-tools-data
+git clone https://alangsto:$GITHUB_KEY@github.com/edx/repo-tools-data.git
+cp repo-tools-data/people.yaml .
+rm -rf repo-tools-data
 echo "Loading identities . . . "
-sudo /usr/bin/python3 /create_identities.py
+/usr/bin/python3 /create_identities.py
 
 sleep 5
 
