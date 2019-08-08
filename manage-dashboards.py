9f82611cb79bdbcdd0a3c58bdb288ff51d082951
Alison Langston <alangsto@wellesley.edu>
2019-08-08 16:48:58 -0400

    removed unnecessary debugging steps and added better output messages

--- manage-dashboards.py
+++ manage-dashboards.py
@@ -14,46 +14,41 @@ def export_dash():
     exports all dashboards stored in the kibana index to the dashboards directory. 
     Filenames will be configured based on dashboard titles.
     '''
-    try:
-        dashboards = search_dashboards(os.environ['ELASTIC_URL'], '.kibana')
+    dashboards = search_dashboards(os.environ['ELASTIC_URL'], '.kibana')
 
-        for dash in dashboards:
-            id = dash['_id'].split(':')[1] #get dashboard id
-            json_name = dash['title'].replace(' ', '-') + "-dashboard.json" #create filename for dashboard
-            file_path = 'dashboards/' + json_name
-            if re.match(r'[-\w_]+\.json',json_name): #check to make sure file being removed has a valid name
-                print('updating existing dashboard')
-                try:
-                    os.remove(file_path)
-                except (FileNotFoundError):
-                    print('saving new dashboard')
-            else:
-                raise Exception('Not a valid file path')
-            export_dashboard( #export using kidash API
-                os.environ['ELASTIC_URL'], #elastic_url
-                id, #dashboard id
-                file_path, #export_file
-                '.kibana',
-                False
-                )
-        print('Exported all dashboards successfully')
-    except (KeyError):
-        raise Exception('Could not export')
+    for dash in dashboards:
+        id = dash['_id'].split(':')[1] #get dashboard id
+        json_name = dash['title'].replace(' ', '-') + "-dashboard.json" #create filename for dashboard
+        file_path = 'dashboards/' + json_name
+        if re.match(r'[-\w_]+\.json',json_name): #check to make sure file being removed has a valid name
+            print('updating existing dashboard: ' + file_path)
+            try:
+                os.remove(file_path)
+            except (FileNotFoundError):
+                print('saving new dashboard: ' + file_path)
+        else:
+            raise Exception('Not a valid file path: ' + file_path)
+        export_dashboard( #export using kidash API
+            os.environ['ELASTIC_URL'], #elastic_url
+            id, #dashboard id
+            file_path, #export_file
+            '.kibana',
+            False
+        )
+    print('Exported all dashboards successfully')
 
 def import_dash():
     '''
     imports all dashboards stored in the dashboards directory to the kibana index. 
     '''
-    try:
-        for file in os.listdir('dashboards'):
-            if file.endswith((".json")):
-                    import_dashboard(
-                        os.environ['ELASTIC_URL'], #elastic_url
-                        os.environ['KIBANA_URL'], #kibana_url
-                        ('dashboards/%s' % file) #import_file
-                        )
-    except (KeyError):
-        raise Exception('Could not import')
+    for file in os.listdir('dashboards'):
+        if file.endswith((".json")):
+            print('Importing dashboard: ' + file)
+            import_dashboard(
+                os.environ['ELASTIC_URL'],
+                os.environ['KIBANA_URL'],
+                ('dashboards/%s' % file)
+            )
             
 
 if __name__ == '__main__':
